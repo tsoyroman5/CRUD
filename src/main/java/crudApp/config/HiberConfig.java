2f7ef6e333d3314cee986aa2615423a095944615
tsoyroman <tsoyroman5@gmail.com>
2022-02-02 21:41:37 +0900

    Update

--- src/main/java/crudApp/config/HiberConfig.java
+++ src/main/java/crudApp/config/HiberConfig.java
@@ -1,8 +1,11 @@
 package crudApp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -15,33 +18,27 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@PropertySource("classpath:app.properties")
 @EnableTransactionManagement
 @ComponentScan(value = "crudApp")
 public class HiberConfig {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/users_db?serverTimezone=UTC";
-    private static final String LOGIN = "root";
-    private static final String PASSWORD = "columbiacollege";
+    @Autowired
+    private Environment env;
 
     @Bean
     public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
         return new PersistenceExceptionTranslationPostProcessor();
     }
 
-    Properties additionalProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        return properties;
-    }
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl(URL);
-        dataSource.setUsername(LOGIN);
-        dataSource.setPassword(PASSWORD);
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
         return dataSource;
     }
 
@@ -52,7 +49,14 @@ public class HiberConfig {
         factoryBean.setDataSource(dataSource());
         factoryBean.setPackagesToScan("crudApp.model");
         factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        factoryBean.setJpaProperties(additionalProperties());
+
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        factoryBean.setJpaProperties(properties);
+
+        factoryBean.setJpaProperties(properties);
         return factoryBean;
     }
 
